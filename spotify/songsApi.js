3aed250fabf1797e5197f954e94910f2ecc76693
Sagi Lefler <sagi.lefler@sap.com>
2018-06-18 12:46:39 +0300

    change config

--- spotify/songsApi.js
+++ spotify/songsApi.js
@@ -13,70 +13,73 @@ var accessToken = token.accessToken;
 
 spotifyApi.setAccessToken(accessToken);
 
-function discoverArtists(kind, genreId, language) {
-  return artistApiCall(kind, genreId, language).then(response =>
-    apiResultToCarousselle(response.data.results)
-  );
+function discoverArtists(artists) {
+    return artistApiCall(artists).then(response =>
+        apiResultToCarousselle(response.data.results)
+)
+    ;
 }
 
-function artistApiCall(kind, genreId, language) {
-    spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-        function(data) {
+function artistApiCall(artists) {
+    // Search artists whose name contains 'Love'
+
+    var artistsString = artists.toString();
+    spotifyApi.searchArtists("elvis","love")
+        .then(function(data) {
+            console.log('Search artists by "Love"', data.body);
+        }, function(err) {
+            console.error(err);
+        });
+
+
+
+    /*spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+        function (data) {
             console.log('Artist albums', data.body);
         },
-        function(err) {
+        function (err) {
             console.error(err);
         }
-    );
-
-
-
-  return axios.get(`https://api.themoviedb.org/3/discover/${kind}`, {
-    params: {
-      api_key: config.MOVIEDB_TOKEN,
-      sort_by: 'popularity.desc',
-      include_adult: false,
-      with_genres: genreId,
-      with_original_language: language,
-    },
-  });
+    );*/
 }
 
 function apiResultToCarousselle(results) {
-  if (results.length === 0) {
+    if (results.length === 0) {
+        return [
+            {
+                type: 'quickReplies',
+                content: {
+                    title: 'Sorry, but I could not find any results for your request :(',
+                    buttons: [{title: 'Start over', value: 'Start over'}],
+                },
+            },
+        ];
+    }
+
+    const cards = results.slice(0, 10).map(e => ({
+        title: e.title || e.name,
+        subtitle: e.overview,
+        imageUrl: `https://image.tmdb.org/t/p/w640${e.poster_path}`,
+        buttons: [
+            {
+                type: 'web_url',
+                value: `https://www.themoviedb.org/movie/${e.id}`,
+                title: 'View More',
+            },
+        ],
+    })
+)
+    ;
+
     return [
-      {
-        type: 'quickReplies',
-        content: {
-          title: 'Sorry, but I could not find any results for your request :(',
-          buttons: [{ title: 'Start over', value: 'Start over' }],
+        {
+            type: 'text',
+            content: "Here's what I found for you!",
         },
-      },
+        {type: 'carousel', content: cards},
     ];
-  }
-
-  const cards = results.slice(0, 10).map(e => ({
-    title: e.title || e.name,
-    subtitle: e.overview,
-    imageUrl: `https://image.tmdb.org/t/p/w640${e.poster_path}`,
-    buttons: [
-      {
-        type: 'web_url',
-        value: `https://www.themoviedb.org/movie/${e.id}`,
-        title: 'View More',
-      },
-    ],
-  }));
-
-  return [
-    {
-      type: 'text',
-      content: "Here's what I found for you!",
-    },
-    { type: 'carousel', content: cards },
-  ];
 }
 
 module.exports = {
-  discoverArtists,
+    discoverArtists,
 };
