7ace4b9eb6125986cf076942fd959d188386c803
Itay Assraf <itay.assraf@sap.com>
2018-06-19 15:37:02 +0300

    ad ui

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var fs = require('fs');
 
 const config = require('./config');
 const spotifyRoute = require('./spotify');
@@ -12,6 +13,32 @@ spotifyRoute(app);
 app.post('/errors', function(req, res) {
   console.log(req.body);
   res.sendStatus(200);
+});
+
+app.get('/',function(req, res) {
+  fs.readFile("public/index.html", function (error, pgResp) {
+    if (error) {
+        res.writeHead(404);
+        res.write('Contents you are looking are Not Found');
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(pgResp);
+    }
+    res.end();
+  });
+});
+
+app.get('/public/Spotify.png',function(req, res) {
+  fs.readFile("public/Spotify.png", function (error, pgResp) {
+    if (error) {
+        res.writeHead(404);
+        res.write('Contents you are looking are Not Found');
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(pgResp);
+    }
+    res.end();
+  });
 });
 
 const port = config.PORT;
